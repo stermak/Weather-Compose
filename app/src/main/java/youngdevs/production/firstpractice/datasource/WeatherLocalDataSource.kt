9e8243820c1d31b6efdd7b08cd7c3761b490583e
Aleksandr Ermakov <7107798@mail.ru>
2023-12-22 01:03:10 +0300

    12345678

--- app/src/main/java/youngdevs/production/firstpractice/datasource/WeatherLocalDataSource.kt
+++ app/src/main/java/youngdevs/production/firstpractice/datasource/WeatherLocalDataSource.kt
@@ -3,6 +3,10 @@ package youngdevs.production.firstpractice.datasource
 import youngdevs.production.firstpractice.data.WeatherData
 
 class WeatherLocalDataSource(private val database: WeatherDatabase) {
+    class WeatherDatabase {
+
+    }
+
     fun getSavedWeather(): WeatherData {
         // Получение сохраненных данных о погоде из локальной БД
         return database.weatherDao().getWeather()
